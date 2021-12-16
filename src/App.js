be6887982b7e0237188bb9f49b1401f8fe2422cf
Pedro A. González <pgonzalez@post.com>
2021-12-15 22:42:59 -0500

    Agregar bootstrap y probar react-bootstrap + axios

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import Alert from 'react-bootstrap/Alert'
+import axios from 'axios'
+
 function App() {
+  axios.get('https://pokeapi.co/api/v2/pokemon/ditto')
+  .then(response => console.log(response.data))
+  .catch(err => console.log(err))
+  
   return (
-    <h1>Hola mundo!</h1>
+    <Alert variant="success" className="m-5">
+  Hola mundo!
+  </Alert>
   );
 }
 
